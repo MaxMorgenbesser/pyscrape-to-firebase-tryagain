a0dd235e7a482d42680d379781429ec965abb2ad
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-25 12:56:10 -0400

    Works

--- main.py
+++ main.py
@@ -39,6 +39,19 @@ def read():
     doc = db.collection(collect).document(docs).get()
     print(doc.to_dict())
 
+def update():
+    collect = input("Please enter the name of the collection you would like to update.\n")
+    docs = input("Please enter the name of the document you would like to update.\n")
+    attrib=input("Please enter the name of the attribute you would like to update.\n")
+    change=input("Please enter what you would like to change the value to. \n")
+    db.collection(collect).document(docs).update({attrib:change})
+
+def delete():
+    collect=input("Please enter the collection you would like to delete from: \n")
+    docs=input("Please enter the document you would like to delete: \n")
+    db.collection(collect).document(docs).delete()
+
+
 def main():
     doCrud = 'Y'
     while doCrud.upper() == 'Y':
@@ -48,5 +61,11 @@ def main():
             doCrud = input('Would you like to create, read, update or delete anything else?\n(y/n): ')
         if crud.upper() == 'R':
             read()
+            doCrud = input('Would you like to create, read, update or delete anything else?\n(y/n): ')
+        if crud.upper() == 'U':
+            update()
+            doCrud = input('Would you like to create, read, update or delete anything else?\n(y/n): ')
+        if crud.upper() == 'D':
+            delete()
             doCrud = input('Would you like to create, read, update or delete anything else?\n(y/n): ')
 main()
